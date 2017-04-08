85b04a9757e2a2adfa8371455f69f5398438e265
Nathan Struhs <nathanstruhs@gmail.com>
2017-04-07 17:49:25 -0700

    Add a full_title helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title='')
+    base_title = "Ecotone - OSU Cascades"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
