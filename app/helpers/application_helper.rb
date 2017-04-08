1da585ad96d23ff498ef73961607ef0d59944b8e
Nathan Struhs <nathanstruhs@gmail.com>
2017-04-08 13:04:46 -0700

    Fix up the page title There was a duplication of Ecotone

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title='')
-    base_title = "Ecotone - OSU Cascades"
+    base_title = "Ecotone | OSU Cascades"
     if page_title.empty?
       base_title
     else
