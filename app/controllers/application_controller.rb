1107f6435c389c7f61cfe9d1d5a3cda7268feefd
Yong Bakos <ybakos@humanoriented.com>
2017-04-13 22:32:33 -0700

    ApplicationController: Replace negative logic with unless.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
   include SessionsHelper
 
   def login_required
-    redirect_to root_path if !logged_in?
+    redirect_to root_path unless logged_in?
   end
+
 end
