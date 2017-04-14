9fab05de985467bdf97782a52e323a2b06a755b6
Nathan Struhs <nathanstruhs@gmail.com>
2017-04-14 10:05:48 -0700

    Fixed #7 seeds.rb creates default admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get     'signup'  => 'users#new'
   get     'login'   => 'sessions#new'
   post    'login'   => 'sessions#create'
-  delete  'logout'  => 'sessions#destroy'  
+  delete  'logout'  => 'sessions#destroy' 
   resources :users
 end
