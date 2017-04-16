bf8117932db9f75bced61c0a2a9265ba64d8e61b
Yong Bakos <ybakos@humanoriented.com>
2017-04-15 18:18:40 -0700

    user: Add comment re Hartl-style, for reference.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,8 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
-   def create_reset_digest
+  # Hartl-Style. See https://www.railstutorial.org/book/password_reset
+  def create_reset_digest
     self.reset_token = User.new_token
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
