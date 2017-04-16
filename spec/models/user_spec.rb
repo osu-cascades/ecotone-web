cb23ee342e0da6e17eea693cb50f6d61fe0c472f
Yong Bakos <ybakos@humanoriented.com>
2017-04-15 18:36:52 -0700

    user_spec: Rephrase test descriptions.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,36 +3,36 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password") }
 
-  it "should be valid" do
+  it "is valid" do
     expect(user.valid?).to be_truthy
   end
 
-  it "name should be present" do
+  it "is invalid without a name" do
     user.name = "  "
     expect(user.valid?).to be_falsy
   end
 
-  it "email should be present" do
+  it "is invalid without an email address" do
     user.email = "  "
     expect(user.valid?).to be_falsy
   end
 
-  it "name should not be too long" do
+  it "is invalid if the name is too long" do
     user.name = 'z' * 51
     expect(user.valid?).to be_falsy
   end
 
-  it "email should not be too long" do
+  it "is invalid if email address is too long" do
     user.email = "z" * 244 + "@example.com"
     expect(user.valid?).to be_falsy
   end
 
-  it "email should not be too short" do
+  it "is invalid if email address is too short" do
     user.email =  "aa"
     expect(user.valid?).to be_falsy
   end
 
-  it "email should be a valid address" do
+  it "is valid given variations of valid email addresses" do
     valid_addresses = ["user@example.com", "USER@foo.COM", "A_US-ER@foo.bar.org", "first.last@foo.jp", "alice+bob@baz.cn"]
     valid_addresses.each do |va|
       user.email = va
@@ -40,7 +40,7 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it "email should reject invalid addresses" do
+  it "is invalid if email address is invalid" do
     invalid_addresses = ["user@example,com", "user_at_foo.org", "user.name@example.", "foo@bar_baz.com", "aoo@bar+baz.com"]
     invalid_addresses.each do |ina|
       user.email = ina
@@ -48,22 +48,22 @@ RSpec.describe User, type: :model do
     end
   end
 
-  it "email should be unique" do
+  it "is invalid if email address is taken" do
     duplicate_user = User.new(name: "Example User", email: user.email, password: "password", password_confirmation: "password")
     expect(duplicate_user.valid?).to be_falsy
   end
 
-  it "password should be present" do
+  it "is invalid without a password" do
     user.password = "   "
     expect(user.valid?).to be_falsy
   end
 
-  it "password should be of sufficient length" do
+  it "is invalid if password is too short" do
     user.password = 'z' * 5
     expect(user.valid?).to be_falsy
   end
 
-  it "password should match password confirmation" do
+  it "is invalid when password does not match password confirmation" do
     user.password = "something"
     user.password_confirmation = "something else"
     expect(user.valid?).to be_falsy
