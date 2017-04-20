964fb329bf802f4c20d81f7f36ce2bc5a94a578f
Yong Bakos <ybakos@humanoriented.com>
2017-04-20 01:03:09 -0700

    Replace page_content_spec with layout test. The feature spec page_content_spec.rb was testing for the presence of content within views. This belongs in the view specs.

---
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "layouts/application", type: :view do
+
+  subject(:layout) { render }
+
+  it { is_expected.to have_title "Ecotone | OSU Cascades" }
+  it { is_expected.to have_content "Ecotone" }
+  it { is_expected.to have_link "Report a Bug" }
+  it { is_expected.to have_link "OSU Cascades" }
+
+  context "not logged in" do
+    it { is_expected.to have_link "Log in" }
+  end
+
+  context "logged in as regular user" do
+
+    let(:user) { build(:user) }
+    before do
+      allow(view).to receive(:current_user) { user }
+    end
+
+    it { is_expected.to_not have_link "Log in" }
+    it { is_expected.to have_link user.name }
+    it { is_expected.to have_link "Log out" }
+    it { is_expected.to have_link "My Account" }
+
+  end
+
+  context "logged in as admin" do
+
+    let(:user) { build(:user, :admin) }
+    before do
+      allow(view).to receive(:current_user) { user }
+    end
+
+    it { is_expected.to have_link "Users" }
+
+  end
+
+end
