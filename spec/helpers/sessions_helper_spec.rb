c0d7594c097aef3d58f269a3702a234f4f1bcd62
Yong Bakos <ybakos@humanoriented.com>
2017-04-20 23:48:28 -0700

    sessions_helper_spec: Add pending stubs.

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,15 +1,29 @@
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SessionsHelper. For example:
-#
-# describe SessionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
-# RSpec.describe SessionsHelper, type: :helper do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe SessionsHelper, type: :helper do
+
+  describe "#log_in" do
+    skip "TODO"
+  end
+
+  describe "#current_user" do
+    skip "TODO"
+  end
+
+  describe "#current_user?" do
+    skip "TODO"
+  end
+
+  describe "#logged_in?" do
+    skip "TODO"
+  end
+
+  describe "#log_out" do
+    skip "TODO"
+  end
+
+  describe "#admin?" do
+    skip "TODO"
+  end
+
+end
