e13e47a79377a118da61eddd5387023fa18db095
Yong Bakos <ybakos@humanoriented.com>
2017-04-21 07:57:19 -0700

    users#index: Add admin label, extract  delete link to helper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def delete_user_link(current_user, user)
+    if admin?(current_user) && !current_user?(user)
+      "| #{link_to "delete", user, method: :delete, data: { confirm: "Delete #{user.name}?" }}".html_safe
+    end
+  end
+
 end
