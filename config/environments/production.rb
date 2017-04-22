f6b2a9d883902d673fd6fa82a3e91b997e479fd5
Nathan Struhs <nathanstruhs@gmail.com>
2017-04-22 09:35:41 -0700

    Update production config to correct mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'osu-ecotone-staging.herokuapp.com'
+  host = 'osu-ecotone.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
