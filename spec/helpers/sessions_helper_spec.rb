85732ac0950a5da99cba3a22231e013270f54d24
Yong Bakos <ybakos@humanoriented.com>
2017-04-22 21:26:07 -0700

    sessions_helper_spec: Test all helper methods. Fixes #19.

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -2,28 +2,90 @@ require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
 
+  let(:user) { create(:user) }
+
   describe "#log_in" do
-    skip "TODO"
+
+    it "stores a user id in the session" do
+      helper.log_in(user)
+      expect(session[:user_id]).to eq(user.id)
+    end
+
   end
 
   describe "#current_user" do
-    skip "TODO"
+
+    it "returns a user whose id is in the session" do
+      session[:user_id] = user.id
+      expect(helper.current_user).to eq(user)
+    end
+
+    it "returns nil when no user id is in the session" do
+      expect(helper.current_user).to be_nil
+    end
+
   end
 
   describe "#current_user?" do
-    skip "TODO"
+
+    it "returns true when the user is the current user" do
+      allow(helper).to receive(:current_user) { user }
+      expect(helper.current_user?(user)).to be true
+    end
+
+    it "returns false when there is no current user" do
+      expect(helper.current_user?(user)).to be false
+    end
+
+    it "returns false when the user is not the current user" do
+      allow(helper).to receive(:current_user) { user }
+      expect(helper.current_user?(build(:user))).to be false
+    end
+
   end
 
   describe "#logged_in?" do
-    skip "TODO"
+
+    it "returns true when there is a current user" do
+      allow(helper).to receive(:current_user) { user }
+      expect(helper.logged_in?).to be true
+    end
+
+    it "returns false when there is no current user" do
+      expect(helper.logged_in?).to be false
+    end
+
   end
 
   describe "#log_out" do
-    skip "TODO"
+
+    before do
+      helper.instance_variable_set(:@current_user, "FAKE")
+      session[:user_id] = "FAKE"
+    end
+
+    it "removes the user_id from the session" do
+      helper.log_out
+      expect(session.has_key? :user_id).to be false
+    end
+
+    it "sets the current user to nil" do
+      helper.log_out
+      expect(helper.instance_variable_get(:@current_user)).to be_nil
+    end
+
   end
 
   describe "#admin?" do
-    skip "TODO"
+
+    it "returns true when user is an admin" do
+      expect(helper.admin?(build(:user, :admin))).to be true
+    end
+
+    it "returns false when the user is not an admin" do
+      expect(helper.admin?(build(:user))).to be false
+    end
+
   end
 
 end
