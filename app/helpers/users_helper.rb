2f6f14a3fdb540ffbbf64a83870fcbc97a8fe119
Nathan Struhs <nathanstruhs@gmail.com>
2017-05-05 18:36:44 -0700

    Fixed #23 Admin delete links are now consistently red

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,7 @@ module UsersHelper
 
   def delete_user_link(current_user, user)
     if admin?(current_user) && !current_user?(user)
-      "| #{link_to "delete", user, method: :delete, data: { confirm: "Delete #{user.name}?" }}".html_safe
+      "| #{link_to "delete", user, method: :delete, data: { confirm: "Delete #{user.name}?" }, class: "lnk-danger"}".html_safe
     end
   end
 
