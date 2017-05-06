52a73055b26839f383d40c86d53a5f9a62371b5e
Nathan Struhs <nathanstruhs@gmail.com>
2017-05-05 18:24:16 -0700

    Fixed #21 refactor shared/_error_messages for all models

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -4,7 +4,7 @@ class Plant < ApplicationRecord
   validates :habitat_type, presence: true
   validates :sunlight_tolerance, presence: true
 
-  has_attached_file :photo, default_url: "missing.png"
+  has_attached_file :photo, default_url: "missing.png", styles: { default: "200x200#", thumbnail: "50x50#" }
   validates_attachment_content_type :photo, content_type: /\Aimage/
 
 end
