5c290d0c73bf10c16562781041d8b3fd90f2373f
Yong Bakos <ybakos@humanoriented.com>
2017-05-13 07:36:17 -0700

    Adjust indentation.

--- db/migrate/20170506165327_rename_sunlight_tolerance_to_tolerance.rb
+++ db/migrate/20170506165327_rename_sunlight_tolerance_to_tolerance.rb
@@ -1,5 +1,5 @@
 class RenameSunlightToleranceToTolerance < ActiveRecord::Migration[5.0]
   def change
-      rename_column :plants, :sunlight_tolerance, :tolerance
+    rename_column :plants, :sunlight_tolerance, :tolerance
   end
 end
