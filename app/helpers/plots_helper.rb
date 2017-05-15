7df0b421b50aa39e297d8cce27576b0424f966ea
Yong Bakos <ybakos@humanoriented.com>
2017-05-15 09:10:38 -0700

    Add helper for rendering featured plant link when it exists.

--- app/helpers/plots_helper.rb
+++ app/helpers/plots_helper.rb
@@ -1,2 +1,11 @@
 module PlotsHelper
+
+  def link_to_featured_plant(plot)
+    if plot.featured_plant
+      link_to plot.featured_plant_name, plant_path(plot.featured_plant)
+    else
+      "Unassigned. #{link_to "Add one?", edit_plot_path(plot)}".html_safe
+    end
+  end
+
 end
