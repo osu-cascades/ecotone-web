0a8aabcfb4d922119be45cf0f45bf2740c0e35d6
Yong Bakos <ybakos@humanoriented.com>
2017-05-16 07:46:17 -0700

    Generalize simplecov config and add filters.

---
+++ .simplecov
@@ -0,0 +1,4 @@
+SimpleCov.start 'rails' do
+  add_filter "application_cable"
+  add_filter "jobs"
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-require 'simplecov'
-SimpleCov.start 'rails'
-
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
 
