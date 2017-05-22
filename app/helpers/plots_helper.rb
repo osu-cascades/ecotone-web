c50502218a493827f9069c2f0592eee4cb0e1340
Nathan Struhs <nathanstruhs@gmail.com>
2017-05-22 12:37:03 -0700

    Restrict public from adding new featured plant to a plot

--- app/helpers/plots_helper.rb
+++ app/helpers/plots_helper.rb
@@ -3,8 +3,10 @@ module PlotsHelper
   def link_to_featured_plant(plot)
     if plot.featured_plant
       link_to plot.featured_plant_name, plant_path(plot.featured_plant)
-    else
+    elsif admin?(current_user)
       "Unassigned. #{link_to "Add one?", edit_plot_path(plot)}".html_safe
+    else
+      "n/a"
     end
   end
 
