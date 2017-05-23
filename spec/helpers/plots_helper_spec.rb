819245c5a856507bff2e9ac3e43e720ddaa3dfcd
Nathan Struhs <nathanstruhs@gmail.com>
2017-05-23 12:13:08 -0700

    Update plots helper spec to accomodate users that are not logged in

--- spec/helpers/plots_helper_spec.rb
+++ spec/helpers/plots_helper_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe PlotsHelper, type: :helper do
   describe "#link_to_featured_plant" do
 
     let(:plot) { build(:plot, id: 1) }
+    let(:current_user) { build(:user, :admin) }
 
-    context "plot has no featured plant" do
+    context "plot has no featured plant and admin is logged in" do
       before { plot.featured_plant = nil }
+      before { allow(helper).to receive(:logged_in?).and_return(true) }
+      before { allow(helper).to receive(:admin?).and_return(true) }
       it "returns 'Unassigned' with a link to edit the plot" do
         expect(helper.link_to_featured_plant(plot)).to match "Unassigned. <a href=\"/plots/1/edit\">Add one?</a>"
+      end
+    end
+
+    context "plot has no featured plant and admin is not logged in" do
+      before { plot.featured_plant = nil }
+      it "returns 'n/a'" do
+        expect(helper.link_to_featured_plant(plot)).to match "n/a"
       end
     end
 
