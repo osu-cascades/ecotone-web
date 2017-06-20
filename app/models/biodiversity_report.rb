11b5f83d76ad55d7fcb16f1e2064acdf8e2f355d
Yong Bakos <ybakos@humanoriented.com>
2017-06-20 00:15:25 -0700

    BiodiversityReport: Add helpers, formats, massage index view. References #60.

--- app/models/biodiversity_report.rb
+++ app/models/biodiversity_report.rb
@@ -1,7 +1,7 @@
 class BiodiversityReport < ApplicationRecord
   belongs_to :user
   belongs_to :plot
-  
+
   has_many :plant_samples, :inverse_of => :biodiversity_report
   accepts_nested_attributes_for :plant_samples
 
@@ -10,5 +10,14 @@ class BiodiversityReport < ApplicationRecord
 
   has_attached_file :photo, default_url: "missing.png", styles: { default: "400x400#", thumbnail: "50x50#" }
   validates_attachment_content_type :photo, content_type: /\Aimage/
+
+  def to_s
+    "Biodiversity Report #{id}"
+  end
+
+  def byline
+    "by #{user} on #{date.to_s(:long)} at #{time.to_s(:ampm)}"
+  end
+
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,9 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
+
+  def to_s
+    name
+  end
+
 end
