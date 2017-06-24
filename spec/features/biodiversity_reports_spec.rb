osu-cascades/ecotone-web

commit e3fd1acddda9a45c92ce8f44ab5803dfa216a8d7

BiodiversityReport #show: Re-order plot and plant samples. References #38, #60.

app/views/biodiversity_reports/show.html.haml
@@ -10,15 +10,15 @@
       %p Biomass Estimate: #{@biodiversity_report.biomass_estimate}
       %p Species Richness: #{@biodiversity_report.species_richness}
       %hr
-      %h3 Plant samples
-      = render(@biodiversity_report.plant_samples) || "No plant samples"
-      %hr
-      %h3 Soil sample
-      - if @biodiversity_report.soil_sample
-        %p PH Level: #{@biodiversity_report.soil_sample.ph_level}
-        %p Temperature: #{@biodiversity_report.soil_sample.temperature}
+      %h3 Soil Sample
+      - if @biodiversitysoil_sample
+        %p PH Level: #{soil_sample.ph_level}
+        %p Temperature: #{soil_sample.temperature}
       - else
         %p No soil sample
+      %hr
+      %h3 Plant Samples
+      = render(@biodiversity_report.plant_samples) || "No plant samples"
       - if @biodiversity_report.editable_by?(current_user)
         .btn-group
           = link_to "Edit", edit_biodiversity_report_path(@biodiversity_report), class: "btn btn-primary edit-btn"

commit cc54393a85ddfa81b66487e33228194da5591985

features: Creating biodiversity report with soil sample. Standardize pH label text. Fix display of soil sample data. - Bug introduced in e3fd1acddda9a45c92ce8f44ab5803dfa216a8d7. Extract report field fill_ins into feature helper method. References #60.

app/views/biodiversity_reports/_soil_sample_fields.html.haml
@@ -1,5 +1,5 @@
 %fieldset
-  = f.label :ph_level
+  = f.label :ph_level, 'pH level'
   = f.text_field :ph_level, class: 'form-control'
   = f.label :temperature
   = f.text_field :temperature, class: 'form-control'

app/views/biodiversity_reports/show.html.haml
@@ -11,9 +11,9 @@
       %p Species Richness: #{@biodiversity_report.species_richness}
       %hr
       %h3 Soil Sample
-      - if @biodiversitysoil_sample
-        %p PH Level: #{soil_sample.ph_level}
-        %p Temperature: #{soil_sample.temperature}
+      - if @biodiversity_report.soil_sample
+        %p pH Level: #{@biodiversity_report.soil_sample.ph_level}
+        %p Temperature: #{@biodiversity_report.soil_sample.temperature}
       - else
         %p No soil sample
       %hr

spec/features/biodiversity_reports_spec.rb
@@ -12,19 +12,16 @@ RSpec.feature "Creating biodiversity reports" do
 
   context "without a soil sample or a plant sample" do
 
-    scenario "as a user providing valid inputs" do
-      select('Plot #1', from: 'Plot')
-      fill_in('Date', :with => '09/11/2001')
-      fill_in('Time', :with => '04:45 PM')
-      fill_in('biodiversity_report_temperature', :with => '72')
-      fill_in('biodiversity_report_biomass_estimate', :with => '50')
-      fill_in('Species richness', :with => '10')
+    scenario "as a user providing valid report data" do
+      fill_in_report_fields
       click_button('Add biodiversity report')
       expect(page).to have_selector ".alert", text: "Biodiversity report was successfully created."
       expect(page).to have_content(BiodiversityReport.last.to_s)
+      click_link(BiodiversityReport.last.to_s)
+      expect(page).to have_content('No soil sample')
     end
 
-    scenario "as a user providing invalid inputs" do
+    scenario "as a user providing invalid report data" do
       click_button('Add biodiversity report')
       expect(page).to have_selector ".alert", text: "The form contains 6 errors."
       page.find("#error_explanation").tap do |error_explanations|
@@ -37,6 +34,43 @@ RSpec.feature "Creating biodiversity reports" do
       end
     end
 
+  end
+
+  context "with a soil sample" do
+
+    before { fill_in_report_fields }
+
+    scenario "as a user providing valid soil sample data" do
+      fill_in('pH level', :with => '10')
+      fill_in('biodiversity_report_soil_sample_attributes_temperature', :with => '100')
+      click_button('Add biodiversity report')
+      expect(page).to have_selector ".alert", text: "Biodiversity report was successfully created."
+      expect(page).to have_content(BiodiversityReport.last.to_s)
+      click_link(BiodiversityReport.last.to_s)
+      expect(page).to_not have_content('No soil sample')
+      expect(page).to have_content('pH Level: 10')
+      expect(page).to have_content('Temperature: 100')
+    end
+
+    scenario "as a user providing invalid soil sample data" do
+      fill_in('pH level', :with => '-1')
+      fill_in('biodiversity_report_soil_sample_attributes_temperature', :with => 'fake')
+      click_button('Add biodiversity report')
+      expect(page).to have_selector ".alert", text: "The form contains 2 errors."
+      expect(page.find("#error_explanation")).to have_content("Soil sample ph level must be greater than or equal to 0")
+      expect(page.find("#error_explanation")).to have_content("Soil sample temperature is not a number")
+      # TODO: soil sample fields should be visible, with values
+    end
+
+  end
+
+  def fill_in_report_fields
+    select('Plot #1', from: 'Plot')
+    fill_in('Date', :with => '09/11/2001')
+    fill_in('Time', :with => '04:45 PM')
+    fill_in('biodiversity_report_temperature', :with => '72')
+    fill_in('biodiversity_report_biomass_estimate', :with => '50')
+    fill_in('Species richness', :with => '10')
   end
 
 end