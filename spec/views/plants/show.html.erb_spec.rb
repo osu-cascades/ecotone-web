a38324d483695d1959baeb988502b47e8f9a8180
Yong Bakos <ybakos@humanoriented.com>
2017-06-29 11:06:29 -0700

    Remove object creation from empty view specs.

--- spec/views/plants/show.html.erb_spec.rb
+++ spec/views/plants/show.html.erb_spec.rb
@@ -1,12 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "plants/show", type: :view do
-  before do
-    @plant = assign(:plant, Plant.create!(
-      :common_name => "Common Name",
-      :scientific_name => "Scientific Name",
-      :habitat_type => "Habitat Type",
-      :sunlight_tolerance => "Sunlight Tolerance"
-    ))
-  end
+
 end
