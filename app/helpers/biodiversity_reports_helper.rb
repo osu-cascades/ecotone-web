6656b5b2d0de5ef35cace538338f92c185c7d5f2
Yong Bakos <ybakos@humanoriented.com>
2017-07-07 18:55:29 -0400

    link_to_add_fields: Clarify parameter names.

--- app/helpers/biodiversity_reports_helper.rb
+++ app/helpers/biodiversity_reports_helper.rb
@@ -16,13 +16,13 @@ module BiodiversityReportsHelper
     soil_sample&.has_temperature_or_ph_level? && 'in' # boostrapV4: 'show'
   end
 
-  def link_to_add_fields(name, f, association)
-    new_object = f.object.send(association).klass.new
+  def link_to_add_fields(link_text, form_builder, association)
+    new_object = form_builder.object.send(association).klass.new
     id = new_object.object_id
-    fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder)
+    fields = form_builder.fields_for(association, new_object, child_index: id) do |fields_builder|
+      render(association.to_s.singularize + "_fields", f: fields_builder)
     end
-    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+    link_to(link_text, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
 end
