8b0fbdbd5a431141b2f70131d2810cf5ef0e304e
Nathan Struhs <nathanstruhs@gmail.com>
2017-07-17 10:11:52 -0700

    References #80, failing test, percent cover should be less than or equal to 100

--- spec/models/plant_sample_spec.rb
+++ spec/models/plant_sample_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe PlantSample, type: :model do
 
   describe "validations and associations" do
     it { is_expected.to validate_numericality_of(:abundance).only_integer.is_greater_than(0) }
-    it { is_expected.to validate_numericality_of(:percent_cover).only_integer.is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:percent_cover).only_integer.is_greater_than(0).is_less_than_or_equal_to(100) }
     it { is_expected.to validate_numericality_of(:biomass_estimate).is_greater_than(0) }
     it { is_expected.to have_attached_file(:photo) }
     it { is_expected.to validate_attachment_content_type(:photo).allowing('image/jpg', 'image/png') }
