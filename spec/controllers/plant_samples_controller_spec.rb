48f85013c2aab848fe7324849848b0eb6fbea3c8
Nathan Struhs <nathanstruhs@gmail.com>
2017-07-21 17:18:33 -0700

    References #85, implement PlantSamplesController#export test

--- spec/controllers/plant_samples_controller_spec.rb
+++ spec/controllers/plant_samples_controller_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 RSpec.describe PlantSamplesController, type: :controller do
 
-  describe "#index" do
+  describe '#index' do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe '#export' do
+    it "returns http success" do
+      get :export, format: :csv
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'generates csv' do
+      get :export, format: :csv
+      expect(response.header['Content-Type']).to include 'application/octet-stream'
+      expect(response.body).to include 'Plot,common_name,scientific_name,measured_on,measured_at,temperature,species_richness,diversity_index,abundance,percent_cover'
     end
   end
 
--- spec/models/plant_sample_spec.rb
+++ spec/models/plant_sample_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe PlantSample, type: :model do
 
   subject(:plant_sample) { build :plant_sample }
 
-  context "when created" do
+  context 'when created' do
     it { is_expected.to have_attributes(abundance: 1, percent_cover: 1) }
     it { is_expected.to be_valid }
   end
 
-  describe "validations and associations" do
+  describe 'validations and associations' do
     it { is_expected.to validate_numericality_of(:abundance).only_integer.is_greater_than(0) }
     it { is_expected.to validate_numericality_of(:percent_cover).only_integer.is_greater_than(0).is_less_than_or_equal_to(100) }
     it { is_expected.to have_attached_file(:photo) }
