710dfc656cbd12b8522daa33cd762b694e3de04d
Yong Bakos <ybakos@humanoriented.com>
2017-07-24 20:27:24 -0700

    FungiSample: Test attachment and attachment validation.

--- spec/models/fungi_sample_spec.rb
+++ spec/models/fungi_sample_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe FungiSample, type: :model do
-  
+
   subject(:fungi_sample) { build :fungi_sample }
 
   context 'when created' do
@@ -15,8 +15,8 @@ RSpec.describe FungiSample, type: :model do
     it { is_expected.to validate_presence_of(:location_within_plot) }
     it { is_expected.to validate_numericality_of(:size).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_presence_of(:description) }
-
+    it { is_expected.to validate_attachment_content_type(:photo).allowing('image/jpg', 'image/png') }
     it { is_expected.to belong_to(:biodiversity_report) }
+    it { is_expected.to have_attached_file(:photo) }
   end
 end
-
