35c2d00dca99708157552db64a4d1ad9e18ea8ad
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-03 13:58:00 -0700

    References #81, set up macroinvertebrate form new/edit forms

--- app/models/macroinvertebrate_sample.rb
+++ app/models/macroinvertebrate_sample.rb
@@ -1,11 +1,13 @@
 class MacroinvertebrateSample < ApplicationRecord
+  ECOSYSTEM_SERVICES = %w[Pollinator Consumer Decomposer]
+
   belongs_to :biodiversity_report
 
   validates :phylum, presence: true
   validates :location_within_plot, presence: true
   validates_numericality_of :quantity, greater_than_or_equal_to: 0
-  validates :ecosystem_service, presence: true, inclusion: { in: %w(pollinator consumer decomposer),
-                                                             message: "%{value} is not a valid ecosystem_service"}
+  validates :ecosystem_service, presence: true, inclusion: { in: ECOSYSTEM_SERVICES,
+                                                             message: "%{value} is not a valid ecosystem service"}
 
   has_attached_file :photo, default_url: "missing.png", styles: { default: "200x200#", thumbnail: "50x50#" }
   validates_attachment_content_type :photo, content_type: /\Aimage/
--- spec/models/macroinvertebrate_sample_spec.rb
+++ spec/models/macroinvertebrate_sample_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe MacroinvertebrateSample, type: :model do
     it { is_expected.to have_attributes(phylum: 'Arthropoda',
                                         location_within_plot: 'on a rock',
                                         quantity: 1,
-                                        ecosystem_service: 'pollinator') }
+                                        ecosystem_service: 'Pollinator') }
     it { is_expected.to be_valid }
   end
 
@@ -16,7 +16,7 @@ RSpec.describe MacroinvertebrateSample, type: :model do
     it { is_expected.to validate_presence_of(:location_within_plot) }
     it { is_expected.to validate_numericality_of(:quantity).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_presence_of(:ecosystem_service) }
-    it { is_expected.to validate_inclusion_of(:ecosystem_service).in_array(%w[pollinator consumer decomposer]) }
+    it { is_expected.to validate_inclusion_of(:ecosystem_service).in_array(%w[Pollinator Consumer Decomposer]) }
     it { is_expected.to validate_attachment_content_type(:photo).allowing('image/jpg', 'image/png') }
     it { is_expected.to have_attached_file(:photo) }
     it { is_expected.to belong_to(:biodiversity_report) }
