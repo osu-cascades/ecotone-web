cdce11042365004f2fb8f08edb28781c6506fb8b
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-10 13:02:52 -0700

    References #82, set up nonvascular plant factory

--- spec/factories/nonvascular_plant_samples.rb
+++ spec/factories/nonvascular_plant_samples.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :nonvascular_plant_sample do
-    location_within_plot "MyString"
-    description "MyString"
-    photo ""
-    biodiversity_report_id 1
+    location_within_plot 'on a twig'
+    description 'description of a nonvascular plant'
+    photo ''
+    biodiversity_report
   end
 end
