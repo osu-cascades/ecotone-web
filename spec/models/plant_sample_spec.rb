d6d8d9b51f30f1cdf171f1f745f1c69e75f4665d
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-11 12:45:58 -0700

    Fixed #92, test PlantSample#to_csv

--- spec/models/plant_sample_spec.rb
+++ spec/models/plant_sample_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe PlantSample, type: :model do
 
   let(:plant) { build :plant }
-  let(:biodiversity_report) { build :biodiversity_report }
+  let(:biodiversity_report) { build :biodiversity_report, measured_on: '2000-01-01', measured_at: '2000-01-01 00:00:00 UTC' }
   subject(:plant_sample) { build :plant_sample, plant: plant, biodiversity_report: biodiversity_report }
 
   context 'when created' do
@@ -21,18 +21,16 @@ RSpec.describe PlantSample, type: :model do
   end
 
   describe '#to_csv' do
-
     it 'generates a valid csv representation' do
-
-      skip 'TODO: add a plant sample to expected.csv and figure out how to test #to_csv on actual
-                  plant sample objects. Current implementation only tests the csv header row'
+      p1 = PlantSample.create(plant: plant, biodiversity_report: biodiversity_report, abundance: 1, percent_cover: 1)
+      p2 = PlantSample.create(plant: plant, biodiversity_report: biodiversity_report, abundance: 1, percent_cover: 1)
 
       plant_sample_attributes = %w(abundance percent_cover)
       bd_attributes = %w(measured_on measured_at temperature species_richness diversity_index)
       plant_attributes = %w(common_name scientific_name)
 
       expected_csv = File.read(File.dirname(__FILE__) + '/expected.csv')
-      generated_csv = PlantSample.all.to_csv(plant_sample_attributes, bd_attributes, plant_attributes)
+      generated_csv = PlantSample.to_csv(plant_sample_attributes, bd_attributes, plant_attributes)
 
       expect(generated_csv).to eq(expected_csv.gsub(/\r/, ''))
     end
