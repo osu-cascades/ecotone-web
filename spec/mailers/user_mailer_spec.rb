53fb334fd8e8f537144973555cf6d76e353f66b8
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-16 16:53:43 -0700

    Clean up spec/mailers

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,26 +1,22 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-
-  describe "password_reset" do
-
+  describe 'password_reset' do
     let(:user) { build(:user, :resetting_password) }
     subject(:mail) do
       UserMailer.password_reset(user)
     end
 
-    it "renders the headers" do
-      expect(mail.subject).to eq("Ecotone Password Reset")
+    it 'renders the headers' do
+      expect(mail.subject).to eq('Ecotone Password Reset')
       expect(mail.to).to eq([user.email])
-      expect(mail.from).to eq(["no-reply@ecotone.osucascades.edu"])
+      expect(mail.from).to eq(['no-reply@ecotone.osucascades.edu'])
     end
 
-    it "renders the body" do
+    it 'renders the body' do
       password_reset_url = /http.*:\/\/.*\/password_resets\/#{user.reset_token}\/edit\?email=#{CGI.escape user.email}/
       expect(mail.body.encoded).to match(password_reset_url)
-      expect(mail.body.encoded).to have_link("Reset password", href: password_reset_url)
+      expect(mail.body.encoded).to have_link('Reset password', href: password_reset_url)
     end
-
   end
-
 end
