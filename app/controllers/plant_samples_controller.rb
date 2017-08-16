c53c58db2cd97f960ad1841d0f4202c55a6a9890
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-16 15:34:17 -0700

    Clean up plant sample controller

--- app/controllers/plant_samples_controller.rb
+++ app/controllers/plant_samples_controller.rb
@@ -1,18 +1,16 @@
 class PlantSamplesController < ApplicationController
-
   def index
     @plant_samples = PlantSample.all
   end
 
   def export
     @data = PlantSample.all
-    plant_sample_attributes = ['abundance', 'percent_cover']
-    bd_attributes = ['measured_on', 'measured_at', 'temperature', 'species_richness', 'diversity_index']
-    plant_attributes = ['common_name', 'scientific_name']
+    plant_sample_attributes = %w[abundance percent_cover]
+    bd_attributes = %w[measured_on measured_at temperature species_richness diversity_index]
+    plant_attributes = %w[common_name scientific_name]
     respond_to do |format|
       format.html
       format.csv { send_data @data.to_csv(plant_sample_attributes, bd_attributes, plant_attributes) }
     end
   end
-
 end
