6ef7489a0910ce8960d384bdcd01aa8471cb280d
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-16 15:41:51 -0700

    Clean up plants controller

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,4 @@
 class PlantsController < ApplicationController
-
   before_action :set_plant, only: [:show, :edit, :update, :destroy, :download_qr]
   before_action :login_required, only: [:new, :create, :edit, :update, :destroy]
   before_action :admin_required, only: [:new, :create, :edit, :update, :destroy]
@@ -8,27 +7,18 @@ class PlantsController < ApplicationController
     @plants = Plant.all
   end
 
-  def show
-  end
-
-  def download_qr
-    @qr = RQRCode::QRCode.new(plant_url(@plant), size: 10, level: :h)
-    png = build_qr_code_image(@qr, 360)
-    send_data(png, type: 'image/png', disposition: 'attachment',
-              filename: "#{@plant.common_name.parameterize(separator: '-')}-qr-code.png")
-  end
+  def show; end
 
   def new
     @plant = Plant.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @plant = Plant.new(plant_params)
     if @plant.save
-      redirect_to plants_path #, {last_added_id: @plant.id }, then create view method
+      redirect_to plants_path # , {last_added_id: @plant.id }, then create view method
       flash[:success] = 'Plant was successfully created.'
     else
       render :new
@@ -50,6 +40,16 @@ class PlantsController < ApplicationController
     flash[:success] = 'Plant was successfully destroyed.'
   end
 
+  def download_qr
+    @qr = RQRCode::QRCode.new(plant_url(@plant), size: 10, level: :h)
+    png = build_qr_code_image(@qr, 360)
+    send_data(png,
+              type: 'image/png',
+              disposition: 'attachment',
+              filename: "#{@plant.common_name.parameterize(separator: '-')}-qr-code.png"
+              )
+  end
+
   private
 
   def set_plant
@@ -60,5 +60,4 @@ class PlantsController < ApplicationController
     params.require(:plant).permit(:common_name, :scientific_name, :description,
      :habitat_type, :tolerance, :photo, :citation, :invasive)
   end
-
 end
