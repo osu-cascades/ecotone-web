62b6601ead0b97a1ddfc2990f020975f6d413c07
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-16 15:49:57 -0700

    Clean up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   before_action :login_required, except: [:new, :create]
   before_action :admin_required, only: :destroy
   before_action :prevent_normal_users_from_editing_and_viewing_other_users, only: [:edit, :update, :show]
@@ -20,7 +19,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if verify_recaptcha(model: @user) && @user.save
       log_in @user
-      flash[:success] = "Welcome to Ecotone!"
+      flash[:success] = 'Welcome to Ecotone!'
       redirect_to @user
     else
       render 'new'
@@ -34,7 +33,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "Account updated"
+      flash[:success] = 'Account updated'
       redirect_to @user
     else
       render 'edit'
@@ -43,7 +42,7 @@ class UsersController < ApplicationController
 
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
+    flash[:success] = 'User deleted'
     redirect_to users_url
   end
 
@@ -56,5 +55,4 @@ class UsersController < ApplicationController
   def prevent_normal_users_from_editing_and_viewing_other_users
     redirect_to(root_url) unless current_user.id == params[:id].to_i || current_user.admin?
   end
-
 end
