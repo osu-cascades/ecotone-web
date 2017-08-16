91b9873df4bfc66f0342a02d89f4c427b27cdf74
Nathan Struhs <nathanstruhs@gmail.com>
2017-08-16 15:48:38 -0700

    Clean up sessions and static pages controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,5 @@
 class SessionsController < ApplicationController
-
-  def new
-  end
+  def new; end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
@@ -10,15 +8,14 @@ class SessionsController < ApplicationController
       flash[:success] = "Welcome back, #{user}."
       redirect_to user
     else
-      flash.now[:danger] = "Invalid email/password combination"
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
 
   def destroy
     log_out
-    flash[:success] = "You have logged out."
+    flash[:success] = 'You have logged out.'
     redirect_to root_url
   end
-
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,3 @@
 class StaticPagesController < ApplicationController
-
-  def home
-  end
-
+  def home; end
 end
