0333f699acef362d3fb62abf7aa7b7133d6371f8
Yong Bakos <ybakos@humanoriented.com>
2017-11-05 12:10:46 -0800

    Style: Deprecate and remove styles. References #90.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def delete_user_link(current_user, user)
     if current_user&.admin? && !current_user?(user)
-      "| #{link_to "delete", user, method: :delete, data: { confirm: "Delete #{user}?" }, class: "lnk-danger", title: "Delete #{user}"}".html_safe
+      "#{link_to 'delete', user, method: :delete, data: { confirm: "Delete #{user}?" }, class: 'btn btn-danger', title: "Delete #{user}"}".html_safe
     end
   end
 
