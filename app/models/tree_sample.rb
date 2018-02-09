2a5bc21494fe65cb29f873ba5bbd6b4d46083c58
Yong Bakos <ybakos@humanoriented.com>
2018-02-09 07:29:36 -0800

    TreeSample: Test model. References #125.

--- app/models/tree_sample.rb
+++ app/models/tree_sample.rb
@@ -1,5 +1,29 @@
 class TreeSample < ApplicationRecord
+
+  enum growth_stage: { # This is more future-proof. Don't use [:composite, :point].
+    seedling: 2,
+    sapling: 4,
+    adult: 6
+  }
+
   belongs_to :plot
-  belongs_to :user
   belongs_to :plant
+  belongs_to :user
+
+  validates :collected_on, presence: true
+  validates_numericality_of :tag_number, only_integer: true, greater_than: 0
+  validates_presence_of :growth_stage
+  validates_numericality_of :dbh, greater_than: 0
+  validates_numericality_of :lower_canopy_height, greater_than: 0
+  validates_numericality_of :upper_canopy_height, greater_than: 0
+  validates_numericality_of :latitude
+  validates_numericality_of :longitude
+
+  has_attached_file :photo, default_url: 'missing.png', styles: { default: '200x200#', thumbnail: '50x50#' }
+  validates_attachment_content_type :photo, content_type: /\Aimage/
+
+  def to_s
+    "#{plant} in #{plot} on #{collected_on} by #{user}"
+  end
+
 end
