2f41b30b9816ded9a06274b9d76e628a4219ac76
Yong Bakos <ybakos@humanoriented.com>
2018-02-09 08:10:27 -0800

    TreeSample: Implement view, controller and CRUD feature spec. References #125.

--- app/controllers/tree_samples_controller.rb
+++ app/controllers/tree_samples_controller.rb
@@ -1,74 +1,71 @@
 class TreeSamplesController < ApplicationController
+  before_action :login_required
+  before_action :admin_required, only: :destroy
   before_action :set_tree_sample, only: [:show, :edit, :update, :destroy]
 
-  # GET /tree_samples
-  # GET /tree_samples.json
   def index
     @tree_samples = TreeSample.all
   end
 
-  # GET /tree_samples/1
-  # GET /tree_samples/1.json
-  def show
-  end
+  def show; end
 
-  # GET /tree_samples/new
   def new
     @tree_sample = TreeSample.new
+    @plots = Plot.all
+    @plants = Plant.all
   end
 
-  # GET /tree_samples/1/edit
   def edit
+    @plots = Plot.all
+    @plants = Plant.all
   end
 
-  # POST /tree_samples
-  # POST /tree_samples.json
   def create
     @tree_sample = TreeSample.new(tree_sample_params)
-
+    @tree_sample.user = current_user
     respond_to do |format|
       if @tree_sample.save
-        format.html { redirect_to @tree_sample, notice: 'Tree sample was successfully created.' }
+        format.html { redirect_to @tree_sample, flash: {success: 'Tree sample was successfully created.'} }
         format.json { render :show, status: :created, location: @tree_sample }
       else
+        @plots = Plot.all
+        @plants = Plant.all
         format.html { render :new }
         format.json { render json: @tree_sample.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /tree_samples/1
-  # PATCH/PUT /tree_samples/1.json
   def update
     respond_to do |format|
       if @tree_sample.update(tree_sample_params)
-        format.html { redirect_to @tree_sample, notice: 'Tree sample was successfully updated.' }
+        format.html { redirect_to @tree_sample, flash: {success: 'Tree sample was successfully updated.'} }
         format.json { render :show, status: :ok, location: @tree_sample }
       else
+        @plots = Plot.all
+        @plants = Plant.all
         format.html { render :edit }
         format.json { render json: @tree_sample.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /tree_samples/1
-  # DELETE /tree_samples/1.json
   def destroy
     @tree_sample.destroy
     respond_to do |format|
-      format.html { redirect_to tree_samples_url, notice: 'Tree sample was successfully destroyed.' }
+      format.html { redirect_to tree_samples_url, flash: {notice: 'Tree sample was successfully destroyed.'} }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_tree_sample
       @tree_sample = TreeSample.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def tree_sample_params
-      params.require(:tree_sample).permit(:collected_on, :plot_id, :user_id, :plant_id, :tag_number, :growth_stage, :dbh, :lower_canopy_height, :upper_canopy_height, :latitude, :longitude, :photo)
+      params.require(:tree_sample).permit(:collected_on, :plot_id, :plant_id, :tag_number, :growth_stage, :dbh, :lower_canopy_height, :upper_canopy_height, :latitude, :longitude, :photo)
     end
+
 end
