852a7b36389efc340a24b3ad60ca4c0a8171480b
Yong Bakos <ybakos@humanoriented.com>
2018-02-10 14:33:54 -0800

    TreeSample: Test optional attribute scenario. References #130.

--- spec/features/user_creates_tree_sample_spec.rb
+++ spec/features/user_creates_tree_sample_spec.rb
@@ -9,19 +9,30 @@ RSpec.feature 'User creates a tree sample' do
     visit new_tree_sample_path
   end
 
-  scenario 'with valid sample attributes' do
-    fill_in('Collection date', with: '09/11/2001')
-    select('Plot #1', from: 'Plot')
-    select('Plant Example', from: 'Species')
-    fill_in('Tag', with: 42)
-    select('Seedling', from: 'Growth stage')
-    fill_in('DBH', with: 1.5)
-    fill_in('Lower canopy height', with: 2.5)
-    fill_in('Upper canopy height', with: 3.5)
-    fill_in('Latitude N', with: 4.5)
-    fill_in('Longitude W', with: 5.5)
-    click_on('Create Tree sample')
-    expect(page).to have_content('Tree sample was successfully created')
+  describe 'with valid sample attributes' do
+
+    before do
+      fill_in('Collection date', with: '09/11/2001')
+      select('Plot #1', from: 'Plot')
+      select('Plant Example', from: 'Species')
+      fill_in('Tag', with: 42)
+      select('Seedling', from: 'Growth stage')
+      fill_in('DBH', with: 1.5)
+    end
+
+    scenario 'and optional values' do
+      fill_in('Lower canopy height', with: 2.5)
+      fill_in('Upper canopy height', with: 3.5)
+      fill_in('Latitude N', with: 4.5)
+      fill_in('Longitude W', with: 5.5)
+      click_on('Create Tree sample')
+      expect(page).to have_content('Tree sample was successfully created')
+    end
+
+    scenario 'without optional values' do
+      click_on('Create Tree sample')
+      expect(page).to have_content('Tree sample was successfully created')
+    end
   end
 
   scenario 'with invalid sample attributes' do
