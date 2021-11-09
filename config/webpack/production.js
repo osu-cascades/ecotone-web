499f07a26ad073775d44dd8b681c14e424a4a55e
Cori Bradford <bradfcor@oregonstate.edu>
2021-11-08 16:45:49 -0800

    comment out all files in config/webpack

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,35 +1,35 @@
-// Note: You must restart bin/webpack-dev-server for changes to take effect
+// // Note: You must restart bin/webpack-dev-server for changes to take effect
 
-/* eslint global-require: 0 */
+// /* eslint global-require: 0 */
 
-const webpack = require('webpack')
-const merge = require('webpack-merge')
-const CompressionPlugin = require('compression-webpack-plugin')
-const sharedConfig = require('./shared.js')
+// const webpack = require('webpack')
+// const merge = require('webpack-merge')
+// const CompressionPlugin = require('compression-webpack-plugin')
+// const sharedConfig = require('./shared.js')
 
-module.exports = merge(sharedConfig, {
-  output: { filename: '[name]-[chunkhash].js' },
-  devtool: 'source-map',
-  stats: 'normal',
+// module.exports = merge(sharedConfig, {
+//   output: { filename: '[name]-[chunkhash].js' },
+//   devtool: 'source-map',
+//   stats: 'normal',
 
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      sourceMap: true,
+//   plugins: [
+//     new webpack.optimize.UglifyJsPlugin({
+//       minimize: true,
+//       sourceMap: true,
 
-      compress: {
-        warnings: false
-      },
+//       compress: {
+//         warnings: false
+//       },
 
-      output: {
-        comments: false
-      }
-    }),
+//       output: {
+//         comments: false
+//       }
+//     }),
 
-    new CompressionPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
-    })
-  ]
-})
+//     new CompressionPlugin({
+//       asset: '[path].gz[query]',
+//       algorithm: 'gzip',
+//       test: /\.(js|css|html|json|ico|svg|eot|otf|ttf)$/
+//     })
+//   ]
+// })
