1308dd149c63a2faee679338b7f7e0eda812e23e
Nikita Rubocki <nikita.rubocki@gmail.com>
2021-11-30 14:27:18 -0800

    Add hyphen into OSU-Cascades titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  BASE_TITLE = 'Ecotone | OSU Cascades'.freeze
+  BASE_TITLE = 'Ecotone | OSU-Cascades'.freeze
 
   def full_title(page_title = '')
     page_title.empty? ? BASE_TITLE : "#{page_title} | #{BASE_TITLE}"
