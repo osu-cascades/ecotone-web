d91640838f22490b6d5b5087f3e48bd2cd730297
Jack Petersen <jack.petersen3@gmail.com>
2021-11-30 16:09:54 -0800

    added image/jpeg to all models so users can upload files in .jpg and .jpeg

--- app/models/tree_sample.rb
+++ app/models/tree_sample.rb
@@ -20,7 +20,7 @@ class TreeSample < ApplicationRecord
   validates_numericality_of :longitude, allow_nil: true
 
   has_one_attached :photo
-  validates :photo, content_type: ['image/jpg', 'image/png']
+  validates :photo, content_type: ['image/jpg', 'image/png', 'image/jpeg']
   def to_s
     "#{plant} in #{plot} on #{collected_on} by #{user}"
   end
