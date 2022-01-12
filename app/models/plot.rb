195b688490cf687f59a31036e2cfb7e97e9d0de4
Jack Petersen <jack.petersen3@gmail.com>
2022-01-12 12:07:06 -0800

    Plot photos: changed model from has_one_attached to has_many attached, changed corresponding views

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -10,7 +10,7 @@ class Plot < ApplicationRecord
   validates :initial_planting_date, presence: true
   validates :initial_succession, presence: true
 
-  has_one_attached :photo
+  has_many_attached :photo
   validates :photo, content_type: ['image/jpg', 'image/png', 'image/jpeg']
 
   belongs_to :featured_plant, class_name: 'Plant', optional: true
