d1a023053f6e4cec4488c219569aed6ec75d5fd2
boykj <jboyk918@gmail.com>
2022-02-03 09:47:26 -0800

    leaflet.rb: Add leaflet formatting file

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,4 @@
+Leaflet.tile_layer = "http://{s}.tile.cloudmade.com/pk.eyJ1IjoiYm95a2oiLCJhIjoiY2t6NjEwZDdhMG13bzJ5cHJ5c2xsbng3NSJ9.lGXEzSMuY3VGPCHJDI5xpg/997/256/{z}/{x}/{y}.png"
+# You can also use any other tile layer here if you don't want to use Cloudmade - see http://leafletjs.com/reference.html#tilelayer for more
+Leaflet.attribution = "Your attribution statement"
+Leaflet.max_zoom = 18
