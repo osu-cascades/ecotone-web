d7ed2c8167ff749751489425005cfc53975089b8
Jack Petersen <jack.petersen3@gmail.com>
2022-02-16 18:17:15 -0800

    Uncommented plant sample csv tests

--- spec/controllers/plant_samples_controller_spec.rb
+++ spec/controllers/plant_samples_controller_spec.rb
@@ -114,10 +114,10 @@ RSpec.describe PlantSamplesController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    # skip 'generates csv' do
-    #   get :export, format: :csv
-    #   expect(response.header['Content-Type']).to include 'application/octet-stream'
-    #   expect(response.body).to include 'Plot,common_name,scientific_name,measured_on,measured_at,temperature,species_richness,diversity_index,abundance,percent_cover'
-    # end
+    skip 'generates csv' do
+      get :export, format: :csv
+      expect(response.header['Content-Type']).to include 'application/octet-stream'
+      expect(response.body).to include 'Plot,common_name,scientific_name,measured_on,measured_at,temperature,species_richness,diversity_index,abundance,percent_cover'
+    end
   end
 end
