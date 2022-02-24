15a9f3ed2fc85b7af07070dab05ac30a05de1b58
NikitaRubocki <nikita.rubocki@gmail.com>
2022-02-24 12:52:18 -0800

    Add titleize for plant name

--- app/helpers/plots_helper.rb
+++ app/helpers/plots_helper.rb
@@ -1,7 +1,7 @@
 module PlotsHelper
   def link_to_featured_plant(plot)
     if plot.featured_plant
-      link_to plot.featured_plant_name, plant_path(plot.featured_plant),
+      link_to plot.featured_plant_name.titleize, plant_path(plot.featured_plant),
         title: "Details of #{plot.featured_plant_name}"
     elsif current_user&.admin?
       "Unassigned. #{link_to "Add one?", edit_plot_path(plot), title: "Edit #{plot}"}".html_safe
