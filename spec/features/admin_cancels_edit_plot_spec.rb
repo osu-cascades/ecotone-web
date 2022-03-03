3e050d7a06d4e44a01d12eaebe8ce79a1f61997d
Jack Petersen <jack.petersen3@gmail.com>
2022-03-02 19:09:59 -0800

    Fixed path in cancel edit plot test

--- spec/features/admin_cancels_edit_plot_spec.rb
+++ spec/features/admin_cancels_edit_plot_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Admin hits cancel' do
-  context 'while creating new plot' do
+  context 'while editing existing plot' do
     let(:user) { create(:user, :admin) }
 
     before do
@@ -9,10 +9,10 @@ RSpec.feature 'Admin hits cancel' do
       create(:plot, id: '1')
     end
 
-    scenario 'after visitng add plots page' do
+    scenario 'after visitng edit plot page' do
       visit edit_plot_path(1)
       click_on('Cancel')
-      expect(page).to have_current_path('/plots1?cancel=Cancel')
+      expect(page).to have_current_path('/plots/1?cancel=Cancel')
     end
   end
 end
