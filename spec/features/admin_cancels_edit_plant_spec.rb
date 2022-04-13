0f4114ac885a705c91c11ae81830589d4a7ddd81
Cori Bradford <bradfcor@oregonstate.edu>
2022-04-13 11:36:49 -0700

    uncomment failing admin tests that were commented to focus on other tests

--- spec/features/admin_cancels_edit_plant_spec.rb
+++ spec/features/admin_cancels_edit_plant_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-# RSpec.feature 'Admin hits cancel' do
-#   context 'while editing existing plant' do
-#     let(:user) { create(:user, :admin) }
+RSpec.feature 'Admin hits cancel' do
+  context 'while editing existing plant' do
+    let(:user) { create(:user, :admin) }
 
-#     before do
-#       sign_in(user)
-#       create(:plant, id: '1')
-#     end
+    before do
+      sign_in(user)
+      create(:plant, id: '1')
+    end
 
-#     scenario 'after visitng edit plant page' do
-#       visit edit_plant_path(1)
-#       click_on('Cancel')
-#       expect(page).to have_current_path('/plants/1?cancel=Cancel')
-#     end
-#   end
-# end
+    scenario 'after visitng edit plant page' do
+      visit edit_plant_path(1)
+      click_on('Cancel')
+      expect(page).to have_current_path('/plants/1?cancel=Cancel')
+    end
+  end
+end
--- spec/features/admin_cancels_edit_plot_spec.rb
+++ spec/features/admin_cancels_edit_plot_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-# RSpec.feature 'Admin hits cancel' do
-#   context 'while editing existing plot' do
-#     let(:user) { create(:user, :admin) }
+RSpec.feature 'Admin hits cancel' do
+  context 'while editing existing plot' do
+    let(:user) { create(:user, :admin) }
 
-#     before do
-#       sign_in(user)
-#       create(:plot, id: '1')
-#     end
+    before do
+      sign_in(user)
+      create(:plot, id: '1')
+    end
 
-#     scenario 'after visitng edit plot page' do
-#       visit edit_plot_path(1)
-#       click_on('Cancel')
-#       expect(page).to have_current_path('/plots/1?cancel=Cancel')
-#     end
-#   end
-# end
+    scenario 'after visitng edit plot page' do
+      visit edit_plot_path(1)
+      click_on('Cancel')
+      expect(page).to have_current_path('/plots/1?cancel=Cancel')
+    end
+  end
+end
