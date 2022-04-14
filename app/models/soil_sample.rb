131251cb23d3b480271e3bb44da20b6ef16fa315
Jack Petersen <jack.petersen3@gmail.com>
2022-04-13 18:46:52 -0700

    remove 'reject if' from accespts_nested_attributes_for in soilsample model

--- app/models/soil_sample.rb
+++ app/models/soil_sample.rb
@@ -10,7 +10,7 @@ class SoilSample < ApplicationRecord
   has_many :nutrients, dependent: :destroy
   has_and_belongs_to_many :biodiversity_reports
 
-  accepts_nested_attributes_for :nutrients, reject_if: :nutrient_presence_unspecified
+  accepts_nested_attributes_for :nutrients#, reject_if: :nutrient_presence_unspecified
 
   validates_presence_of :collected_on
   validates_presence_of :collection_method
@@ -36,7 +36,7 @@ class SoilSample < ApplicationRecord
 
   private
   def all_nutrients_selected
-    errors.add(:nutrients, "must all have values") if nutrients.blank?
+    errors.add(:nutrients, "must all have values") if nutrients.map{ |n| n}.blank?
   end
 
   def nutrient_presence_unspecified(attributes)
