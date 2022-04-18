9bd0471cd9186c92f49c78570c8f6ae57a29bfeb
NikitaRubocki <nikita.rubocki@gmail.com>
2022-04-17 17:29:58 -0700

    Make users alphabetical

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   before_action :prevent_normal_users_from_viewing_other_users, only: [:show]
 
   def index
-    @users = User.all
+    @users = User.all.order(name: :asc)
   end
 
   def show
