b84aac8420f0af65fc4d747197c99c99a93eb7a4
Cori Bradford <bradfcor@oregonstate.edu>
2022-04-18 11:00:25 -0700

    remove admin generation, add loop for generation of 20 plants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
-user = User.create!( name:  "Administrator",
-                     email: "admin@example.com",
-                     password:              "password",
-                     password_confirmation: "password",
-                     admin: true )
+
+20.times do |plant|
+    Plant.create!(
+        common_name: "Lorem Ipsum#{plant}",
+        scientific_name: "Lorem ipsum#{plant}",
+        description: "Lorem ipsum dolor sit amet, consectetur 
+        adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua. Ut enim ad minim 
+            veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat.",
+        habitat_type: "Lorem ipsum dolor sit amet.",
+        tolerance: "Lorem ipsum dolor sit amet."
+    )
+        end
